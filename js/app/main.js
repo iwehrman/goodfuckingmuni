a562ab2257619452f0e9b10ed2714880e9a6c440
Ian Wehrman <wehrman@adobe.com>
2013-06-13 23:25:05 -0700

    pushState

--- js/app/main.js
+++ js/app/main.js
@@ -1,8 +1,13 @@
-/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*jslint vars: true, plusplus: true, browser: true, nomen: true, indent: 4, maxerr: 50 */
 /*global define, $, PathUtils */
 
 define(["jquery", "jquery.alpha", "jquery.beta"], function ($) {
     "use strict";
+    
+    var $body = $("body"),
+        $stops = $body.find(".content-stops"),
+        $directions = $body.find(".content-directions"),
+        $routelist = $body.find(".content-routes");
     
     function commandURL(command) {
         var baseURL = "http://webservices.nextbus.com/service/publicXMLFeed?a=sf-muni",
@@ -104,10 +109,8 @@ define(["jquery", "jquery.alpha", "jquery.beta"], function ($) {
         return deferred.promise();
     }
     
-    function showStopsForDirection(route, tag, $prev) {
+    function showStopsForDirection(route, tag) {
         var direction = route.directions[tag],
-            $body = $("body"),
-            $stops = $body.find(".content-stops"),
             $container = $("<div class='topcoat-list__container'></div>"),
             $header = $("<h2 class='topcoat-list__header'>" + direction.title + "</h2>"),
             $list = $("<ul class='topcoat-list'></ul>");
@@ -120,14 +123,14 @@ define(["jquery", "jquery.alpha", "jquery.beta"], function ($) {
         
         $container.append($header).append($list);
         $stops.append($container);
-        $prev.hide();
         $stops.show();
+        
+        var stateObj = { route: route, direction: tag };
+        history.pushState(stateObj, "Route: " + route.title, "#route=" + route.tag + "&direction=" + tag);
     }
     
-    function showDirections(route, $prev) {
-        var $body = $("body"),
-            $directions = $body.find(".content-directions"),
-            $container = $("<div class='topcoat-list__container'></div>"),
+    function showDirections(routes, route) {
+        var $container = $("<div class='topcoat-list__container'></div>"),
             $header = $("<h2 class='topcoat-list__header'>" + route.title + "</h2>"),
             $list = $("<ul class='topcoat-list route-directions'></ul>"),
             direction,
@@ -143,23 +146,25 @@ define(["jquery", "jquery.alpha", "jquery.beta"], function ($) {
         
         $container.append($header).append($list);
         $directions.append($container);
-        $prev.hide();
         $directions.show();
+        
+        var stateObj = { routes: routes, route: route };
+        history.pushState(stateObj, "Route: " + route.title, "#route=" + route.tag);
         
         $directions.find(".entry-direction").each(function (i, d) {
             var $direction = $(d),
                 tag = $direction.data("tag");
             
             $direction.on("click", function () {
-                showStopsForDirection(route, tag, $directions);
+                $directions.hide();
+                $directions.empty();
+                showStopsForDirection(route, tag);
             });
         });
     }
-    
+
     function showRoutes(routes) {
-        var $body = $("body"),
-            $routelist = $body.find(".content-routes"),
-            $container = $("<div class='topcoat-list__container'></div>"),
+        var $container = $("<div class='topcoat-list__container'></div>"),
             $header = $("<h2 class='topcoat-list__header'>Routes</h2>"),
             $list = $("<ul class='topcoat-list'></ul>");
 
@@ -170,6 +175,8 @@ define(["jquery", "jquery.alpha", "jquery.beta"], function ($) {
         
         $container.append($header).append($list);
         $routelist.append($container);
+        $routelist.show();
+        history.pushState({ routes: routes }, "Routes", "/");
         
         $routelist.find(".entry-route").each(function (i, r) {
             var $route = $(r),
@@ -177,15 +184,32 @@ define(["jquery", "jquery.alpha", "jquery.beta"], function ($) {
             
             $route.on("click", function () {
                 getRoute(tag).done(function (route) {
-                    showDirections(route, $routelist);
+                    $routelist.hide();
+                    $routelist.empty();
+                    showDirections(routes, route);
                 });
             });
         });
     }
     
+    window.onpopstate = function (event) {
+        var state = event.state;
+        
+        if (state.route) {
+            $stops.hide();
+            $stops.empty();
+            history.pushState({ routes: state.routes }, "Routes", "/");
+            showDirections(state.routes, state.route);
+        } else if (state.routes) {
+            $directions.hide();
+            $directions.empty();
+            showRoutes(state.routes);
+        }
+        
+    };
+    
     var routesPromise = getRoutes();
     
-    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
     $(function () {
         //$('body').alpha().beta();
         routesPromise.done(showRoutes);
