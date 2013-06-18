881b97ac36de1e359c4dbb64825fa5cca7975fa1
Ian Wehrman <wehrman@adobe.com>
2013-06-17 23:33:43 -0700

    cache command results

--- js/app/command.js
+++ js/app/command.js
@@ -3,6 +3,14 @@
 
 define(["jquery", "app/geolocation"], function ($, geolocation) {
     "use strict";
+
+    var ROUTE_LIST_TIMEOUT = 1000 * 60 * 60 * 24,
+        ROUTE_TIMEOUT = 1000 * 60,
+        PREDICTION_TIMEOUT = 1000 * 30;
+    
+    var cachedRouteList = null,
+        cachedRoutes = {},
+        cachedPredictions = {};
     
     var locationPromise = geolocation.getLocation();
        
@@ -52,130 +60,165 @@ define(["jquery", "app/geolocation"], function ($, geolocation) {
     function getRoutes() {
         var deferred = $.Deferred();
         
-        cmdRouteList().done(function (data) {
-            var routes  = [];
+        if (cachedRouteList) {
+            deferred.resolve(cachedRouteList);
+        } else {
+            cmdRouteList().done(function (data) {
+                var routes  = [];
+                
+                $(data).find("route").each(function (i, r) {
+                    var $route  = $(r),
+                        tag     = $route.attr("tag"),
+                        title   = $route.attr("title");
+                        
+                    routes.push({tag: tag, title: title});
+                });
+                
+                cachedRouteList = routes;
+                setTimeout(function () {
+                    cachedRouteList = null;
+                }, ROUTE_LIST_TIMEOUT);
+                
+                deferred.resolve(routes);
+            }).fail(deferred.reject.bind(deferred));
             
-            $(data).find("route").each(function (i, r) {
-                var $route  = $(r),
-                    tag     = $route.attr("tag"),
-                    title   = $route.attr("title");
-                    
-                routes.push({tag: tag, title: title});
-            });
-            deferred.resolve(routes);
-        }).fail(deferred.reject.bind(deferred));
-        
+
+        }
         return deferred.promise();
     }
     
     function getRoute(tag) {
         var deferred = $.Deferred();
         
-        cmdRouteConfig(tag, true).done(function (data) {
-            var $data       = $(data),
-                $route      = $data.find("route"),
-                tag         = $route.attr("tag"),
-                title       = $route.attr("title"),
-                color       = $route.attr("color"),
-                opposite    = $route.attr("oppositeColor"),
-                directions  = {},
-                allStops    = {};
-            
-            locationPromise.done(function (position, timestamp) {
-                $route.children("stop").each(function (i, s) {
-                    var $stop   = $(s),
-                        id      = parseInt($stop.attr("stopId"), 10),
-                        tag     = $stop.attr("tag"),
-                        title   = $stop.attr("title"),
-                        lat     = parseFloat($stop.attr("lat")),
-                        lon     = parseFloat($stop.attr("lon")),
-                        stopPos = {lat: lat, lon: lon},
-                        dist    = geolocation.distance(position, stopPos);
-                    
-                    allStops[tag] = {
-                        title:  title,
-                        lat:    lat,
-                        lon:    lon,
-                        dist:   dist
-                    };
-                });
+        if (cachedRoutes[tag]) {
+            deferred.resolve(cachedRoutes[tag]);
+        } else {
+            cmdRouteConfig(tag, true).done(function (data) {
+                var $data       = $(data),
+                    $route      = $data.find("route"),
+                    tag         = $route.attr("tag"),
+                    title       = $route.attr("title"),
+                    color       = $route.attr("color"),
+                    opposite    = $route.attr("oppositeColor"),
+                    directions  = {},
+                    allStops    = {};
                 
-                $route.children("direction").each(function (i, d) {
-                    var $direction = $(d),
-                        tag = $direction.attr("tag"),
-                        title = $direction.attr("title"),
-                        name = $direction.attr("name"),
-                        stops = [];
-                    
-                    var minDist = Number.POSITIVE_INFINITY,
-                        maxDist = Number.NEGATIVE_INFINITY;
-                    
-                    $direction.children("stop").each(function (i, s) {
-                        var $stop = $(s),
-                            stopTag = $stop.attr("tag"),
-                            stop = allStops[stopTag];
+                locationPromise.done(function (position, timestamp) {
+                    $route.children("stop").each(function (i, s) {
+                        var $stop   = $(s),
+                            id      = parseInt($stop.attr("stopId"), 10),
+                            tag     = $stop.attr("tag"),
+                            title   = $stop.attr("title"),
+                            lat     = parseFloat($stop.attr("lat")),
+                            lon     = parseFloat($stop.attr("lon")),
+                            stopPos = {lat: lat, lon: lon},
+                            dist    = geolocation.distance(position, stopPos);
                         
-                        stops.push(stopTag);
-                        
-                        if (stop.dist > maxDist) {
-                            maxDist = stop.dist;
-                        }
-                        
-                        if (stop.dist < minDist) {
-                            minDist = stop.dist;
-                        }
+                        allStops[tag] = {
+                            title:  title,
+                            lat:    lat,
+                            lon:    lon,
+                            dist:   dist
+                        };
                     });
                     
-                    directions[tag] = {
-                        title:  title,
-                        name:   name,
-                        stops:  stops,
-                        maxDist: maxDist,
-                        minDist: minDist
+                    $route.children("direction").each(function (i, d) {
+                        var $direction = $(d),
+                            tag = $direction.attr("tag"),
+                            title = $direction.attr("title"),
+                            name = $direction.attr("name"),
+                            stops = [];
+                        
+                        var minDist = Number.POSITIVE_INFINITY,
+                            maxDist = Number.NEGATIVE_INFINITY;
+                        
+                        $direction.children("stop").each(function (i, s) {
+                            var $stop = $(s),
+                                stopTag = $stop.attr("tag"),
+                                stop = allStops[stopTag];
+                            
+                            stops.push(stopTag);
+                            
+                            if (stop.dist > maxDist) {
+                                maxDist = stop.dist;
+                            }
+                            
+                            if (stop.dist < minDist) {
+                                minDist = stop.dist;
+                            }
+                        });
+                        
+                        directions[tag] = {
+                            title:  title,
+                            name:   name,
+                            stops:  stops,
+                            maxDist: maxDist,
+                            minDist: minDist
+                        };
+                    });
+                    var route = {
+                        tag:            tag,
+                        title:          title,
+                        color:          color,
+                        oppositeColor:  opposite,
+                        directions:     directions,
+                        stops:          allStops
                     };
-                });
-                
-                deferred.resolve({
-                    tag:            tag,
-                    title:          title,
-                    color:          color,
-                    oppositeColor:  opposite,
-                    directions:     directions,
-                    stops:          allStops
-                });
-                
+                    
+                    cachedRoutes[tag] = route;
+                    setTimeout(function () {
+                        cachedRoutes[tag] = null;
+                    }, ROUTE_TIMEOUT);
+                    
+                    deferred.resolve(route);
+                    
+                }).fail(deferred.reject.bind(deferred));
             }).fail(deferred.reject.bind(deferred));
-        }).fail(deferred.reject.bind(deferred));
-            
+        }
+
         return deferred.promise();
     }
     
     function getPredictions(routeTag, stopTag) {
         var deferred = $.Deferred();
         
-        cmdPredictions(routeTag, stopTag).done(function (data) {
-            var predictions  = [];
-            
-            $(data).find("prediction").each(function (i, p) {
-                var $prediction  = $(p),
-                    dirTag = $prediction.attr("dirTag"),
-                    epochTime = parseInt($prediction.attr("epochTime"), 10),
-                    seconds = parseInt($prediction.attr("seconds"), 10),
-                    minutes = parseInt($prediction.attr("minutes"), 10),
-                    isDeparture = $prediction.attr("isDeparture") === "true",
-                    affectedByLayover = $prediction.attr("affectedByLayover") === "true";
-                    
-                predictions.push({
-                    dirTag: dirTag,
-                    epochTime: epochTime,
-                    seconds: seconds,
-                    minutes: minutes,
-                    isDeparture: isDeparture,
-                    affectedByLayover: affectedByLayover
+        if (!cachedPredictions[routeTag]) {
+            cachedPredictions[routeTag] = {};
+        }
+
+        if (cachedPredictions[routeTag][stopTag]) {
+            deferred.resolve(cachedPredictions[routeTag][stopTag]);
+        } else {
+            cmdPredictions(routeTag, stopTag).done(function (data) {
+                var predictions  = [];
+                
+                $(data).find("prediction").each(function (i, p) {
+                    var $prediction  = $(p),
+                        dirTag = $prediction.attr("dirTag"),
+                        epochTime = parseInt($prediction.attr("epochTime"), 10),
+                        seconds = parseInt($prediction.attr("seconds"), 10),
+                        minutes = parseInt($prediction.attr("minutes"), 10),
+                        isDeparture = $prediction.attr("isDeparture") === "true",
+                        affectedByLayover = $prediction.attr("affectedByLayover") === "true";
+                        
+                    predictions.push({
+                        dirTag: dirTag,
+                        epochTime: epochTime,
+                        seconds: seconds,
+                        minutes: minutes,
+                        isDeparture: isDeparture,
+                        affectedByLayover: affectedByLayover
+                    });
                 });
-            });
-            deferred.resolve(predictions);
-        }).fail(deferred.reject.bind(deferred));
+                
+                cachedPredictions[routeTag][stopTag] = predictions;
+                setTimeout(function () {
+                    cachedPredictions[routeTag][stopTag] = null;
+                }, PREDICTION_TIMEOUT);
+                    
+                deferred.resolve(predictions);
+            }).fail(deferred.reject.bind(deferred));
+        }
         
         return deferred.promise();
     }
