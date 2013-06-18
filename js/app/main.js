42741987727e6037098b3fd7916bcb374c7c9c18
Ian Wehrman <wehrman@adobe.com>
2013-06-18 00:16:54 -0700

    refactoring and simplifying; don't scroll when backing up; remove temp stop storage

--- js/app/main.js
+++ js/app/main.js
@@ -8,163 +8,159 @@ define(["jquery", "app/storage", "app/command"], function ($, storage, command) 
         $stops = $body.find(".content-stops"),
         $directions = $body.find(".content-directions"),
         $routelist = $body.find(".content-routes"),
-        $predictions = $body.find(".content-predictions"),
-        routesPromise;
+        $predictions = $body.find(".content-predictions");
     
-    function showPredictions(routes, route, dirTag, stopTag, predictions) {
-        var stop = route.stops[stopTag],
-            direction = route.directions[dirTag],
-            $container = $("<div class='topcoat-list__container'></div>"),
-            $header = $("<h2 class='topcoat-list__header'>" + route.title +
-                        " &gt; " + direction.title + " &gt; " + stop.title + "</h2>"),
-            $list = $("<ul class='topcoat-list'></ul>");
-        
-        predictions.forEach(function (prediction, index) {
-            var $item = $("<li class='topcoat-list__item entry-prediction'>"),
-                $text = $("<span>").append(prediction.minutes + " minutes");
-            
-            if (index === 0) {
-                $text.addClass("closest");
-            }
-            
-            $item.append($text);
-            $list.append($item);
+    function showPredictions(routeTag, dirTag, stopTag) {
+        command.getRoute(routeTag).done(function (route) {
+            command.getPredictions(routeTag, stopTag).done(function (predictions) {
+                var stop = route.stops[stopTag],
+                    direction = route.directions[dirTag],
+                    $container = $("<div class='topcoat-list__container'></div>"),
+                    $header = $("<h2 class='topcoat-list__header'>" + route.title +
+                                " &gt; " + direction.title + " &gt; " + stop.title + "</h2>"),
+                    $list = $("<ul class='topcoat-list'></ul>");
+                
+                predictions.forEach(function (prediction, index) {
+                    var $item = $("<li class='topcoat-list__item entry-prediction'>"),
+                        $text = $("<span>").append(prediction.minutes + " minutes");
+                    
+                    if (index === 0) {
+                        $text.addClass("closest");
+                    }
+                    
+                    $item.append($text);
+                    $list.append($item);
+                });
+                
+                $container.append($header).append($list);
+                $predictions.append($container);
+                $predictions.show();
+            });
         });
-        
-        $container.append($header).append($list);
-        $predictions.append($container);
-        $predictions.show();
     }
         
-    function showStops(routes, route, dirTag) {
-        var direction = route.directions[dirTag],
-            $container = $("<div class='topcoat-list__container'></div>"),
-            $header = $("<h2 class='topcoat-list__header'>" + route.title +
-                        " &gt; " + direction.title + "</h2>"),
-            $list = $("<ul class='topcoat-list'></ul>"),
-            storedStops = storage.getStoredStops(route.tag, dirTag);
-        
-        function normalizeDist(direction, stop) {
-            var range = direction.maxDist - direction.minDist,
-                fromMin = stop.dist - direction.minDist;
+    function showStops(routeTag, dirTag, scroll) {
+        command.getRoute(routeTag).done(function (route) {
+            var direction = route.directions[dirTag],
+                $container = $("<div class='topcoat-list__container'></div>"),
+                $header = $("<h2 class='topcoat-list__header'>" + route.title +
+                            " &gt; " + direction.title + "</h2>"),
+                $list = $("<ul class='topcoat-list'></ul>");
             
-            return 1 - (fromMin / range);
-        }
-        
-        direction.stops.forEach(function (stopTag) {
-            var stop = route.stops[stopTag],
-                norm = normalizeDist(direction, stop),
-                $item = $("<li class='topcoat-list__item entry-stop' data-tag='" +
-                          stopTag + "'>"),
-                $text = $("<span>").append(stop.title);
-            
-            if (norm === 1) {
-                $text.addClass("closest");
+            function normalizeDist(direction, stop) {
+                var range = direction.maxDist - direction.minDist,
+                    fromMin = stop.dist - direction.minDist;
+                
+                return 1 - (fromMin / range);
             }
             
-            if (storedStops[stopTag]) {
-                $text.addClass("stored");
-            }
-            
-            $item.on("click", function () {
-                if (storedStops[stopTag]) {
-                    $text.removeClass("stored");
-                    storedStops = storage.forgetStop(route.tag, dirTag, stopTag);
-                } else {
-                    $text.addClass("stored");
-                    storedStops = storage.rememberStop(route.tag, dirTag, stopTag);
+            direction.stops.forEach(function (stopTag) {
+                var stop = route.stops[stopTag],
+                    norm = normalizeDist(direction, stop),
+                    $item = $("<li class='topcoat-list__item entry-stop' data-tag='" +
+                              stopTag + "'>"),
+                    $text = $("<span>").append(stop.title);
+                
+                if (norm === 1) {
+                    $text.addClass("closest");
                 }
-                command.getPredictions(route.tag, stopTag).done(function (predictions) {
-                    var stateObj = { routes: routes, route: route, dirTag: dirTag, stopTag: stopTag };
-                    history.pushState(stateObj, null, "#r=" + route.tag + "&d=" + dirTag + "&s=" + stopTag);
+                
+                $item.on("click", function () {
+                    var stateObj = { routeTag: routeTag, dirTag: dirTag, stopTag: stopTag };
+                    history.pushState(stateObj, null, "#r=" + routeTag + "&d=" + dirTag + "&s=" + stopTag);
                     
                     $stops.hide();
                     $stops.empty();
-                    showPredictions(routes, route, dirTag, stopTag, predictions);
+                    showPredictions(routeTag, dirTag, stopTag);
                 });
+                
+                $item.append($text);
+                $list.append($item);
             });
             
-            $item.append($text);
-            $list.append($item);
-        });
-        
-        $container.append($header).append($list);
-        $stops.append($container);
-        $stops.show();
-        
-        $body.animate({
-            scrollTop: $list.find(".closest").parent().offset().top - $body.scrollTop()
+            $container.append($header).append($list);
+            $stops.append($container);
+            $stops.show();
+            
+            if (scroll) {
+                $body.animate({
+                    scrollTop: $list.find(".closest").parent().offset().top - $body.scrollTop()
+                });
+            }
         });
     }
     
-    function showDirections(routes, route) {
-        var $container = $("<div class='topcoat-list__container'></div>"),
-            $header = $("<h2 class='topcoat-list__header'>" + route.title + "</h2>"),
-            $list = $("<ul class='topcoat-list route-directions'></ul>"),
-            direction,
-            dirTag;
-        
-        for (dirTag in route.directions) {
-            if (route.directions.hasOwnProperty(dirTag)) {
-                direction = route.directions[dirTag];
-                
-                $list.append("<li class='topcoat-list__item entry-direction' data-tag='" +
-                         dirTag + "'>" + direction.title + "</li>");
-            }
-        }
-        
-        $container.append($header).append($list);
-        $directions.append($container);
-        $directions.show();
-        
-        $directions.find(".entry-direction").each(function (i, d) {
-            var $direction = $(d),
-                dirTag = $direction.data("tag");
+    function showDirections(routeTag) {
+        command.getRoute(routeTag).done(function (route) {
+            var $container = $("<div class='topcoat-list__container'></div>"),
+                $header = $("<h2 class='topcoat-list__header'>" + route.title + "</h2>"),
+                $list = $("<ul class='topcoat-list route-directions'></ul>"),
+                direction,
+                dirTag,
+                $item,
+                $text;
             
-            $direction.on("click", function () {
-                var stateObj = { routes: routes, route: route, dirTag: dirTag };
-                history.pushState(stateObj, null, "#r=" + route.tag + "&d=" + dirTag);
+            function clickHandler(dirTag) {
+                return function () {
+                    var stateObj = { routeTag: routeTag, dirTag: dirTag };
+                    history.pushState(stateObj, null, "#r=" + routeTag + "&d=" + dirTag);
+                    
+                    $directions.hide();
+                    $directions.empty();
+                    showStops(routeTag, dirTag, true);
+                };
+            }
+            
+            for (dirTag in route.directions) {
+                if (route.directions.hasOwnProperty(dirTag)) {
+                    direction = route.directions[dirTag];
+                    $item = $("<li class='topcoat-list__item entry-direction' data-tag='" +
+                             dirTag + "'>");
+                    $text = $("<span>").append(direction.title);
                 
-                $directions.hide();
-                $directions.empty();
-                showStops(routes, route, dirTag);
-            });
+                    $item.append($text);
+                    $list.append($item);
+                    
+                    $item.on("click", clickHandler(dirTag));
+                }
+            }
+            
+            $container.append($header).append($list);
+            $directions.append($container);
+            $directions.show();
         });
-        
-        
     }
 
-    function showRoutes(routes) {
-        var $container = $("<div class='topcoat-list__container'></div>"),
-            $header = $("<h2 class='topcoat-list__header'>Routes</h2>"),
-            $list = $("<ul class='topcoat-list'></ul>");
-
-        routes.forEach(function (route) {
-            $list.append("<li class='topcoat-list__item entry-route' data-tag='" +
-                         route.tag + "'>" + route.title + "</li>");
-        });
-        
-        $container.append($header).append($list);
-        $routelist.append($container);
-        $routelist.show();
-        
-        $routelist.find(".entry-route").each(function (i, r) {
-            var $route = $(r),
-                tag = $route.data("tag");
-            
-            $route.on("click", function () {
-                command.getRoute(tag).done(function (route) {
-                    var stateObj = { routes: routes, route: route };
-                    history.pushState(stateObj, null, "#r=" + route.tag);
-                    
-                    $routelist.hide();
-                    $routelist.empty();
-                    showDirections(routes, route);
+    function showRoutes() {
+        command.getRoutes().done(function (routes) {
+            var $container = $("<div class='topcoat-list__container'></div>"),
+                $header = $("<h2 class='topcoat-list__header'>Routes</h2>"),
+                $list = $("<ul class='topcoat-list'></ul>");
+    
+            routes.forEach(function (route) {
+                var $item = $("<li class='topcoat-list__item entry-route' data-tag='" +
+                             route.tag + "'>"),
+                    $text = $("<span>").append(route.title);
+                
+                $item.append($text);
+                $list.append($item);
+                
+                $item.on("click", function () {
+                    command.getRoute(route.tag).done(function (route) {
+                        var stateObj = { routeTag: route.tag };
+                        history.pushState(stateObj, null, "#r=" + route.tag);
+                        
+                        $routelist.hide();
+                        $routelist.empty();
+                        showDirections(route.tag);
+                    });
                 });
             });
+            
+            $container.append($header).append($list);
+            $routelist.append($container);
+            $routelist.show();
         });
-        
-        
     }
     
     window.onpopstate = function (event) {
@@ -174,29 +170,44 @@ define(["jquery", "app/storage", "app/command"], function ($, storage, command) 
             if (state.dirTag) {
                 $predictions.hide();
                 $predictions.empty();
-                showStops(state.routes, state.route, state.dirTag);
-            } else if (state.route) {
+                showStops(state.routeTag, state.dirTag);
+            } else if (state.routeTag) {
                 $stops.hide();
                 $stops.empty();
-                showDirections(state.routes, state.route);
-            } else if (state.routes) {
-                $directions.hide();
-                $directions.empty();
-                showRoutes(state.routes);
+                showDirections(state.routeTag);
             }
+        } else {
+            $directions.hide();
+            $directions.empty();
+            showRoutes();
         }
     };
-    
-    routesPromise = command.getRoutes();
 
     $(function () {
-        //$('body').alpha().beta();
+        var hash = window.location.hash;
+                
+        if (hash) {
+            var params = hash.substring(1).split("&").reduce(function (obj, eq) {
+                var terms = eq.split("=");
+                obj[terms[0]] = terms[1];
+                return obj;
+            }, {});
+            
+            if (params.r) {
+                if (params.d) {
+                    if (params.s) {
+                        showPredictions(params.r, params.d, params.s);
+                    } else {
+                        showStops(params.r, params.d, true);
+                    }
+                } else {
+                    showDirections(params.r);
+                }
+                return;
+            }
+        }
         
-        routesPromise.done(function (routes) {
-            var stateObj = { routes: routes };
-            history.pushState(stateObj, null, "");
-            showRoutes(routes);
-        });
+        showRoutes();
     });
     
 });
