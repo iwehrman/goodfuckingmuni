de7e80c6a0580f847b6919c27357641919987cae
Ian Wehrman <wehrman@adobe.com>
2013-07-07 19:25:14 -0700

    Add config for r.js optimizer

---
+++ js/build.js
@@ -0,0 +1,13 @@
+({
+    baseUrl: ".",
+	paths: {
+        data: "../data",
+        html: "../html",
+        jquery: "lib/jquery.min",
+        async: "lib/async",
+        text: "lib/text",
+        mustache: "lib/mustache"
+	},
+	name: "app.js",
+	out: "app-built.js"
+})
