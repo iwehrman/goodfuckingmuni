8fc19e5eaf335f00423d8a277991a5c9ed28f43c
Ian Wehrman <iwehrman@gmail.com>
2013-07-09 08:40:44 -0700

    update build.js for touchSwipe

--- js/build.js
+++ js/build.js
@@ -4,6 +4,7 @@
         data: "../data",
         html: "../html",
         jquery: "lib/jquery.min",
+        "jquery.touchSwipe": "lib/jquery.touchSwipe",
         async: "lib/async",
         text: "lib/text",
         mustache: "lib/mustache"
