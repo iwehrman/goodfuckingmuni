1f0410c0c034b134647eadffb566acf273069db5
Ian Wehrman <iwehrman@gmail.com>
2013-09-01 10:31:43 -0700

    Fix the cache manifest for use with google maps

--- js/build.js
+++ js/build.js
@@ -7,9 +7,11 @@
         "jquery.event.move": "lib/jquery.event.move",
         "jquery.event.swipe": "lib/jquery.event.swipe",
         async: "lib/async",
+        rasync: "lib/rasync",
         text: "lib/text",
         mustache: "lib/mustache"
     },
+    include: ["rasync"],
     name: "app.js",
     out: "app-built.js",
     preserveLicenseComments: false
