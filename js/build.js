ed5471b9f3103ec3fbb760b1d051716510beaa6b
Ian Wehrman <wehrman@adobe.com>
2014-04-18 11:57:22 -0700

    Compile JS and build production/debug html files at install time

--- js/build.js
+++ js/build.js
@@ -3,17 +3,17 @@
     paths: {
         data: "../data",
         html: "../html",
-        jquery: "components/jquery/jquery.min",
-        "jquery.event.move": "components/jquery.event.move/js/jquery.event.move",
-        "jquery.event.swipe": "components/jquery.event.swipe/js/jquery.event.swipe",
-        rasync: "components/requirejs-plugins/src/async",
-        text: "components/requirejs-text/text",
-        mustache: "components/mustache/mustache",
-        q: "components/q/q",
-        fastclick: "components/fastclick/lib/fastclick"
+        jquery: "./components/jquery/jquery.min",
+        "jquery.event.move": "./components/jquery.event.move/js/jquery.event.move",
+        "jquery.event.swipe": "./components/jquery.event.swipe/js/jquery.event.swipe",
+        rasync: "./components/requirejs-plugins/src/async",
+        text: "./components/requirejs-text/text",
+        mustache: "./components/mustache/mustache",
+        q: "./components/q/q",
+        fastclick: "./components/fastclick/lib/fastclick"
     },
     include: ["rasync"],
-    name: "app.js",
-    out: "app-built.js",
+    name: "./app",
+    out: "./app-built.js",
     preserveLicenseComments: false
 })
